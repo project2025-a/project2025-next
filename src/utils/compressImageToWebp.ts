353f247e448f176a3325bba99378767c8d089f15
hayanLee <hayanlee.dev@gmail.com>
2025-09-07 22:37:07 +0900

    fix : webp -> jpeg 변환
    - 이름은 .webp 이지만 실제로는 jpeg 파일로 변환됨

--- src/utils/compressImageToWebp.ts
+++ src/utils/compressImageToWebp.ts
@@ -1,6 +1,7 @@
 /* 이미지 압축 + webp 변환*/
 import imageCompression, { Options } from 'browser-image-compression';
 import heic2any from 'heic2any';
+import { returnFileSize } from './returnFileSize';
 
 // heic → jpeg 변환
 export async function convertHeicToJPEG(file: File): Promise<File> {
@@ -10,7 +11,7 @@ export async function convertHeicToJPEG(file: File): Promise<File> {
     quality: 0.8,
   });
 
-  return new File([convertedBlob as Blob], file.name.replace(/\.heic$/i, '.jpeg'), {
+  return new File([convertedBlob as Blob], file.name.replace(/\.heic$/i, '.webp'), {
     type: 'image/jpeg',
   });
 }
@@ -22,7 +23,7 @@ export async function compressImageToWebp(
   const defaultOptions: Partial<Options> = {
     maxSizeMB: 3,
     useWebWorker: true,
-    fileType: 'image/webp',
+    fileType: 'image/jpeg',
     maxWidthOrHeight: 1600,
     ...options,
   };
@@ -30,14 +31,17 @@ export async function compressImageToWebp(
   try {
     // 변환 + 압축
     if (file.type === 'image/heic' || file.name.endsWith('.heic')) {
-      // console.log('원본', returnFileSize(file.size), file.type);
+      console.log('원본', returnFileSize(file.size), file.type);
 
       // 1. HEIC → JPEG 변환
       const jpegFile = await convertHeicToJPEG(file);
       // console.log('변환된 JPEG', returnFileSize(jpegFile.size), jpegFile.type);
+      // console.log('jpegFile', jpegFile);
 
       // 2. 변환된 JPEG 파일 압축
-      return await imageCompression(jpegFile, defaultOptions);
+      const compressedFile = await imageCompression(jpegFile, defaultOptions);
+      // console.log('compressedFile', compressedFile);
+      return compressedFile;
     }
     const compressedFile = await imageCompression(file, defaultOptions);
     return compressedFile;
